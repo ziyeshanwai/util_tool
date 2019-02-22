3e431a03bb2060e4fd17d1f4de09fa28f03f04f0
LiyouWang <108506687@qq.com>
2019-02-22 16:28:17 +0800

    与loadandwriteobj.py 是一样的 提供读和写obj的基本功能

--- util.py
+++ util.py
@@ -1,77 +1,64 @@
-import os
-import numpy as np
-import pickle as pickle
+
+def loadObj(path):
+    """Load obj file
+    读取三角形和四边形的mesh
+    返回vertex和face的list
+    """
+    if path.endswith('.obj'):
+        f = open(path, 'r')
+        lines = f.readlines()
+        vertics = []
+        faces = []
+        for line in lines:
+            if line.startswith('v') and not line.startswith('vt') and not line.startswith('vn'):
+                line_split = line.split()
+                ver = line_split[1:4]
+                ver = [float(v) for v in ver]
+                # print(ver)
+                vertics.append(ver)
+            else:
+                if line.startswith('f'):
+                    line_split = line.split()
+                    if '/' in line:  # 根据需要这里补充obj的其他格式解析
+                        tmp_faces = line_split[1:]
+                        f = []
+                        for tmp_face in tmp_faces:
+                            f.append(int(tmp_face.split('/')[0]))
+                        faces.append(f)
+                    else:
+                        face = line_split[1:]
+                        face = [int(fa) for fa in face]
+                        faces.append(face)
+        return vertics, faces
+
+    else:
+        print('格式不正确，请检查obj格式')
+        return
 
 
-def load_binary_pickle( filepath ):
-    with open( filepath, 'rb' ) as f:
-        data = pickle.load( f )
-    return data
+def writeObj(file_name_path, vertexs, faces):
+    """write the obj file to the specific path
+       file_name_path:保存的文件路径
+       vertexs:顶点数组 list
+       faces: 面 list
+    """
+    with open(file_name_path, 'w') as f:
+        for v in vertexs:
+            # print(v)
+            f.write("v {} {} {}\n".format(v[0], v[1], v[2]))
+        for face in faces:
+            if len(face) == 4:
+                f.write("f {} {} {} {}\n".format(face[0], face[1], face[2], face[3])) # 保存四个顶点
+            if len(face) == 3:
+                f.write("f {} {} {}\n".format(face[0], face[1], face[2])) # 保存三个顶点
+        print("saved mesh to {}".format(file_name_path))
 
 
-def save_binary_pickle( data, filepath ):
-    with open( filepath, 'wb' ) as f:
-        pickle.dump( data, f )
-
-'''
-加载简单的obj文件,只有顶点和边
-v 1.5 6.3 3.5
-...
-f 1 2 3
-f 4 5 6
-这种
-'''
-def load_simple_obj(filename):
-    f = open(filename, 'r')
-
-    def get_num(string, type):
-        if type == 'int':
-            return int(string)
-        elif type == 'float':
-            return float(string)
-        else:
-            print('Wrong type specified')
-
-    vertices = []
-    faces = []
-
-    for line in f:
-        if '//' in line:
-            str = line.split()
-            len_str = len(str)
-            for i in range(len_str):
-                if '//' in str[i]:
-                    str[i] = str[i].split('//')[0]
-        else:
-            str = line.split()
-        if len(str) == 0:
-            continue
-
-        if str[0] == '#':
-            continue
-        elif str[0] == 'v':
-            tmp_v = [get_num(s, 'float') for s in str[1:]]
-            vertices.append( tmp_v )
-
-        elif str[0] == 'f':
-            tmp_f = [get_num(s, 'int')-1 for s in str[1:]]
-            faces.append( tmp_f )
-
-    f.close()
-    return ( np.asarray(vertices), np.asarray(faces) )
-
-
-def write_simple_obj( mesh_v, mesh_f, filepath, verbose=False ):
-    with open( filepath, 'w') as fp:
-        for v in mesh_v:
-            fp.write( 'v %f %f %f\n' % ( v[0], v[1], v[2]) )
-        for f in mesh_f+1: # Faces are 1-based, not 0-based in obj files
-            fp.write( 'f %d %d %d\n' %  (f[0], f[1], f[2]) )
-    if verbose:
-        print('mesh saved to: ', filepath)
-
-
-def safe_mkdir( file_dir ):
-    if not os.path.exists( file_dir ):
-        os.mkdir( file_dir ) 
-
+if __name__ == '__main__':
+    '''unit test'''
+    #path = 'C:\\Users\\Administrator\\Desktop\\xiaoyue_OBJ_Seq\\xiaoyue.0001.obj'
+    path = './test.obj'
+    v, f = loadObj(path)
+    save_path = './test1.obj'
+    writeObj(save_path, v, f)
+    # print(f)
