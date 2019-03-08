f2340b8901574736aba65623e8f8fd5b67df11a8
LiyouWang <108506687@qq.com>
2019-03-08 15:41:48 +0800

    流程化处理blendshape迁移

--- util.py
+++ util.py
@@ -4,7 +4,6 @@ import pickle
 import json
 import scipy.io as io
 import numpy as np
-import numpy.linalg as LA
 from align_trajectory import align_sim3
 
 
@@ -351,8 +350,7 @@ def ExtractFaceVertexIndex(Face_verts, Head_verts, err=1e-9):
         h_tmp = h_v - v
         hv_sum = np.sum(h_tmp**2, axis=1)
         ind = np.where(hv_sum <= err)
-        # print(ind)
-        assert len(ind) >= 1, '点序为空'
+        assert len(ind[0]) >= 1, '点序为空, 人脸数据可能头部数据不匹配'
         index.append(ind[0][0])
     assert len(index) == len(f_v), "索引与面部数据长度不一致"
     return index
@@ -511,12 +509,145 @@ def BatchAlignFacewithFixedPoints(FaceToalignPath, FaceAligned_verts, SavePath, 
             aligned_v = AlignTwoFaceWithFixedPoints(FaceAligned_verts, ToAlign_v, points_index)
             writeObj(os.path.join(SavePath, filename), aligned_v, ToAlign_f)
 
+"""
+迁移的整体流程：
+step1、对齐所有演员的blendshape
+step2、生成三角网格、从头部提取脸部
+step3、标记点、面部表情迁移
+step4、将对齐的面部还原为头部，将头部还原为四边形
+"""
+def BasicBlenshapeTransfer_step1(Actor_head_path, ActorNeutralHeadPoseName, CharactorHeadPath,
+                           CharactorHeadNeutralPoseName, ActorFaceNeutralPosePath, ActorFaceNeutralPoseName,
+                           CharactorFaceNeutralPosePath, CharactorFaceNeutralPoseName, rootPath):
+    """
+    生成迁移所需要的所有基本元素 主要有以下几个功能：
+    1、将演员和角色的四边形头部转换为三角形同时保存对应的index.pkl 文件
+    2、将演员和角色的三角形面部提取出来并保存 面部点序face_index.pkl 文件
+    目录结构如下:
+    actor
+    |------Tri
+    |       |-------Face
+    |       |         |---------NeutralPose(wrap 标记点 points.txt存放的地方)
+    |       |         |---------Blendshapes(迁移使用的文件夹, 包含NeutralPose)
+    |       |-------Head
+    |       |         |---------NeutralPose(标准头部模型，用来还原除面部的区域)
+    |       |         |---------Blendshapes(存放稳定脸部后的头部结果)
+    |------Quad(存放由三角形还原为四边形还原点文件以及迁移完成后的四边形结果)
+    :param Actor_head_path:演员的头部的obj文件夹路径
+    :param ActorNeutralHeadPoseName:演员的自然表情的名字
+    :param CharactorHeadPath:角色头部的obj文件夹路径
+    :param CharactorHeadNeutralPoseName:角色头部文件名称
+    :param ActorFaceNeutralPosePath:演员面部自然表情的路径
+    :param ActorFaceNeutralPoseName:演员面部自然表情的名字 要求是三角面
+    :param CharactorFaceNeutralPosePath:角色面部的自然表情路径
+    :param CharactorFaceNeutralPoseName:角色的面部自然表情名称 要求是三角面
+    :param rootPath:文件生成的根目录
+    :return: None
+    """
+    if not os.path.isdir(rootPath):
+        os.mkdir(rootPath)
+    if not os.path.isdir(os.path.join(rootPath, "Actor")):
+        os.mkdir(os.path.join(rootPath, "Actor"))
+    if not os.path.isdir(os.path.join(rootPath, "Charactor")):
+        os.mkdir(os.path.join(rootPath, "Charactor"))
+    if not os.path.isdir(os.path.join(rootPath, "Actor", "Tri")):
+        os.mkdir(os.path.join(rootPath, "Actor", "Tri"))
+    if not os.path.isdir(os.path.join(rootPath, "Charactor", "Tri")):
+        os.mkdir(os.path.join(rootPath, "Charactor", "Tri"))
+    if not os.path.isdir(os.path.join(rootPath, "Actor", "Quad")):
+        os.mkdir(os.path.join(rootPath, "Actor", "Quad"))
+    if not os.path.isdir(os.path.join(rootPath, "Charactor", "Quad")):
+        os.mkdir(os.path.join(rootPath, "Charactor", "Quad"))
+    if not os.path.isdir(os.path.join(rootPath, "Actor", "Tri", "Face")):
+        os.mkdir(os.path.join(rootPath, "Actor", "Tri", "Face"))
+    if not os.path.isdir(os.path.join(rootPath, "Charactor", "Tri", "Face")):
+        os.mkdir(os.path.join(rootPath, "Charactor", "Tri", "Face"))
+    if not os.path.isdir(os.path.join(rootPath, "Actor", "Tri", "Head")):
+        os.mkdir(os.path.join(rootPath, "Actor", "Tri", "Head"))
+    if not os.path.isdir(os.path.join(rootPath, "Charactor", "Tri", "Head")):
+        os.mkdir(os.path.join(rootPath, "Charactor", "Tri", "Head"))
+    if not os.path.isdir(os.path.join(rootPath, "Actor", "Tri", "Face", "NeutralPose")):
+        os.mkdir(os.path.join(rootPath, "Actor", "Tri", "Face", "NeutralPose"))
+    if not os.path.isdir(os.path.join(rootPath, "Charactor", "Tri", "Face", "NeutralPose")):
+        os.mkdir(os.path.join(rootPath, "Charactor", "Tri", "Face", "NeutralPose"))
+    if not os.path.isdir(os.path.join(rootPath, "Actor", "Tri", "Face", "Blendshapes")):
+        os.mkdir(os.path.join(rootPath, "Actor", "Tri", "Face", "Blendshapes"))
+    if not os.path.isdir(os.path.join(rootPath, "Charactor", "Tri", "Face", "Blendshapes")):
+        os.mkdir(os.path.join(rootPath, "Charactor", "Tri", "Face", "Blendshapes"))
+    if not os.path.isdir(os.path.join(rootPath, "Actor", "Tri", "Head", "NeutralPose")):
+        os.mkdir(os.path.join(rootPath, "Actor", "Tri", "Head", "NeutralPose"))
+    if not os.path.isdir(os.path.join(rootPath, "Charactor", "Tri", "Head", "NeutralPose")):
+        os.mkdir(os.path.join(rootPath, "Charactor", "Tri", "Head", "NeutralPose"))
+    if not os.path.isdir(os.path.join(rootPath, "Actor", "Tri", "Head", "Blendshapes")):
+        os.mkdir(os.path.join(rootPath, "Actor", "Tri", "Head", "Blendshapes"))
+    if not os.path.isdir(os.path.join(rootPath, "Charactor", "Tri", "Head", "Blendshapes")):
+        os.mkdir(os.path.join(rootPath, "Charactor", "Tri", "Head", "Blendshapes"))
+    if not os.path.isdir(os.path.join(rootPath, "Charactor", "Tri", "Face", "AlignedBlendshapes")):
+        os.mkdir(os.path.join(rootPath, "Charactor", "Tri", "Face", "AlignedBlendshapes"))
+
+    Actor_Neutral_Head = os.path.join(Actor_head_path, ActorNeutralHeadPoseName)
+    Actor_Neutral_Head_v, Actor_Neutral_Head_f = loadObj(Actor_Neutral_Head)
+    Actor_tv, Actor_tf, backup_ind = Quad2Tri(Actor_Neutral_Head_v, Actor_Neutral_Head_f)
+    writeObj(os.path.join(rootPath, "Actor", "Tri", "Head", "NeutralPose", ActorNeutralHeadPoseName), Actor_tv, Actor_tf)
+    save_pickle_file(os.path.join(rootPath, "Actor", "Tri", "Head", "NeutralPose", "index.pkl"), backup_ind)
+    file_dir = Actor_head_path
+    filenames = os.listdir(file_dir)
+    target_dir = os.path.join(rootPath, "Actor", "Tri", "Head", "Blendshapes")
+    ind_list = batch_convert_quad2tri(file_dir, filenames, target_dir)  # 四边形转化为三件形
+    print("演员四边形面转化为三角形面完成..")  # 之后应该手动选取三角面的人脸
+    if os.path.isfile(os.path.join(ActorFaceNeutralPosePath, ActorFaceNeutralPoseName)):  # 如果头部的脸部区域有提前准备好
+        Actor_neutral_face = os.path.join(ActorFaceNeutralPosePath, ActorFaceNeutralPoseName)
+        Actor_neutral_face_v, Actor_neutral_face_f = loadObj(Actor_neutral_face)
+        writeObj(os.path.join(rootPath, "Actor", "Tri", "Face", "NeutralPose", ActorFaceNeutralPoseName),
+                 Actor_neutral_face_v, Actor_neutral_face_f)
+        face_index = ExtractFaceVertexIndex(Actor_neutral_face_v, Actor_Neutral_Head_v)  # 面部区域占头部区域的点序位置
+        save_pickle_file(os.path.join(rootPath, "Actor", "Tri", "Face", "NeutralPose", "face_index.pkl"), face_index)
+        batch_convert_head_to_face(Actor_head_path, face_index, Actor_neutral_face_f, os.path.join(rootPath, "Actor", "Tri", "Face", "Blendshapes"))  # 提取所有面部区域
+        print("演员的所有blendshape面部数据提取完毕..")
+    Charactor_Neutral_Head = os.path.join(CharactorHeadPath, CharactorHeadNeutralPoseName)
+    Charactor_Neutral_Head_v, Charactor_Neutral_Head_f = loadObj(Charactor_Neutral_Head)
+    Charactor_tv, Charactor_tf, backup_ind = Quad2Tri(Charactor_Neutral_Head_v, Charactor_Neutral_Head_f)
+    writeObj(os.path.join(rootPath, "Charactor", "Tri", "Head", "NeutralPose", CharactorHeadNeutralPoseName), Charactor_tv,
+             Charactor_tf)
+    save_pickle_file(os.path.join(rootPath, "Charactor", "Tri", "Head", "NeutralPose", "index.pkl"), backup_ind)
+    if os.path.isfile(os.path.join(CharactorFaceNeutralPosePath, CharactorFaceNeutralPoseName)):  # 如果头部的脸部区域有提前准备好
+        Charactor_neutral_face = os.path.join(CharactorFaceNeutralPosePath, CharactorFaceNeutralPoseName)
+        Charactor_neutral_face_v, Charactor_neutral_face_f = loadObj(Charactor_neutral_face)
+        writeObj(os.path.join(rootPath, "Charactor", "Tri", "Face", "NeutralPose", CharactorFaceNeutralPoseName),
+                 Charactor_neutral_face_v, Charactor_neutral_face_f)
+        face_index = ExtractFaceVertexIndex(Charactor_neutral_face_v, Charactor_Neutral_Head_v)  # 面部区域占头部区域的点序位置
+        save_pickle_file(os.path.join(rootPath, "Charactor", "Tri", "Face", "NeutralPose", "face_index.pkl"), face_index)
+
+
+def BasicBlenshapeTransfer_step3(Charactor_transfered_face_path, aligned_face, face_index_path, Charactor_head, index_path,
+                                 AlignPoints_index, AlignedFaces_path_tosave, Tri_Head_path_tosave, Quad_Head_path_tosave):
+    """
+    将迁移完的结果角色面部对齐并安回到固定的头部同时生成四边形的头部
+    :param Charactor_transfered_face_path: 迁移完的角色面部的结果路径
+    :param aligned_face: 对齐的人脸路径
+    :param face_index_path: 面部顶点点序路径
+    :param Charactor_head: 需要使用的头部路径
+    :param index_path: 三角形还原四边形的还原点路径
+    :param AlignPoints_index: 面部对齐所使用的固定点
+    :param AlignedFaces_path_tosave: 面部对齐结果要保存的路径
+    :param Tri_Head_path_tosave: 三角形头部要保存的路径
+    :param Quad_Head_path_tosave: 四边形头部要保存的路径
+    :return:
+    """
+    Face_aligned_v, Face_aligned_f = loadObj(aligned_face)
+    BatchAlignFacewithFixedPoints(Charactor_transfered_face_path, Face_aligned_v, AlignedFaces_path_tosave, AlignPoints_index)
+    standard_head_v, standard_head_f = loadObj(Charactor_head)
+    face_index = load_pickle_file(face_index_path)
+    _, _ = batch_convert_face_to_head(AlignedFaces_path_tosave, Tri_Head_path_tosave, standard_head_v, standard_head_f, face_index)
+    backup_ind = load_pickle_file(index_path)
+    to_convert_tri_head_names = os.listdir(Tri_Head_path_tosave)
+    _, _ = batch_convert_tri2quad(Tri_Head_path_tosave, to_convert_tri_head_names, Quad_Head_path_tosave, backup_ind)
 
 def VTK_show(f_v, f_f, tri=True):
     """
     显示物体
-    :param f_v:
-    :param f_f:
+    :param f_v: 顶点
+    :param f_f: 点序
     :return: 无
     """
     if tri:
@@ -611,11 +742,11 @@ if __name__ == '__main__':
     """
     批量将迁移过后的pose的三角形mesh 转化为四边形面片
     """
-    file_dir = "D:\\Blendshape-Based Animation\\charactor\\Chactor_Au_aigned_tri"
-    filenames = os.listdir(file_dir)
-    target_dir = "D:\\Blendshape-Based Animation\\charactor\\Charactor_Au_aligned_quads"
-    index_list = load_pickle_file('D:\\Blendshape-Based Animation\\charactor\\index.pkl')
-    qv, qf = batch_convert_tri2quad(file_dir, filenames, target_dir, index_list)
+    # file_dir = "D:\\Blendshape-Based Animation\\charactor\\Chactor_Au_aigned_tri"
+    # filenames = os.listdir(file_dir)
+    # target_dir = "D:\\Blendshape-Based Animation\\charactor\\Charactor_Au_aligned_quads"
+    # index_list = load_pickle_file('D:\\Blendshape-Based Animation\\charactor\\index.pkl')
+    # qv, qf = batch_convert_tri2quad(file_dir, filenames, target_dir, index_list)
 
     """
     去均值化
@@ -640,13 +771,13 @@ if __name__ == '__main__':
     """
     批量提取面部数据与还原测试
     """
-    # face_v, face_f = loadObj(os.path.join('D:\\Blendshape-Based Animation\\Alien\\NP_alien\\face', 'Alien_face.obj'))
-    # head_v, head_f = loadObj(os.path.join('D:\\Blendshape-Based Animation\\Alien\\NP_alien\\head', 'alien_head_tri.obj'))
+    # face_v, face_f = loadObj(os.path.join('D:\\Blendshape-Animation\\Transfered\\Small_sister1\\Charactor\\Tri\\Face\\NeutralPose', 'Xiaoyue.face.obj'))
+    # head_v, head_f = loadObj(os.path.join('D:\\Blendshape-Animation\\Transfered\\Small_sister1\\Charactor\\Tri\\Head\\NeutralPose', 'Xiaoyue_quad.obj'))
     # index = ExtractFaceVertexIndex(face_v, head_v)  # 确定face_index
-    # save_pickle_file(os.path.join('D:\\Blendshape-Based Animation\\Alien\\NP_alien\\head', 'face_index.pkl'), index)
-    # # head_path = 'D:\\Blendshape-Based Animation\\triangemesh-source'
-    # face_path = 'D:\\Blendshape-Based Animation\\Alien\\aligned_face'
-    # # f_v, f_f = batch_convert_head_to_face(head_path, index, face_f, face_path)
+    # # save_pickle_file(os.path.join('D:\\Blendshape-Based Animation\\Alien\\NP_alien\\head', 'face_index.pkl'), index)
+    # head_path = 'D:\\Blendshape-Based Animation\\charactor\\charactor_triangles'
+    # face_path = 'D:\\Blendshape-Animation\\Transfered\\Small_sister1\\Charactor\\Tri\\Face\\Blendshapes'
+    # f_v, f_f = batch_convert_head_to_face(head_path, index, face_f, face_path)
     # head_path_tosave = 'D:\\Blendshape-Based Animation\\Alien\\Transerfed_Head_Tri_mesh'
     # h_v, h_f = batch_convert_face_to_head(face_path, head_path_tosave, head_v, head_f, index)
 
@@ -674,6 +805,53 @@ if __name__ == '__main__':
     # # fixedPoints_index = [1900, 7969, 5271]  # 对齐外星人用的索引
     # fixedPoints_index = [16438, 2977, 984, 14115, 14916, 5011]  # 小姐姐
     # BatchAlignFacewithFixedPoints(face_path, face_v, saved_path, fixedPoints_index)
+    """
+    step0:对齐blendshape
+    """
+
+    """
+    step1:生成表情迁移的基本目录结构以及演员和角色的三角mesh
+    """
+    Actor_head_path = "D:\\Blendshape-Animation\\Models\\Sources\\quad_mesh"
+    ActorNeutralHeadPoseName = "xiaoyue.0013.obj"
+    CharactorHeadPath = "D:\\Blendshape-Animation\\Models\\Targets\\Xiaoyue"
+    CharactorHeadNeutralPoseName = "Xiaoyue_quad.obj"
+    ActorFaceNeutralPosePath = "D:\\Blendshape-Animation\\Transfered\\Small_sister\\Actor\\Tri\\Face\\NeutralPose"
+    ActorFaceNeutralPoseName = "xiaoyue.0013.face.obj"
+    CharactorFaceNeutralPosePath = "D:\\Blendshape-Animation\\Transfered\\Small_sister1\\Charactor\\Tri\\Face\\NeutralPose"  # 要求三角面
+    CharactorFaceNeutralPoseName = "Xiaoyue.face.obj"  # 不能写none
+    rootPath = os.path.join("D:\\Blendshape-Animation\\Transfered", "Small_sister1")
+    # BasicBlenshapeTransfer_step1(Actor_head_path, ActorNeutralHeadPoseName, CharactorHeadPath,
+    #                        CharactorHeadNeutralPoseName, ActorFaceNeutralPosePath, ActorFaceNeutralPoseName,
+    #                        CharactorFaceNeutralPosePath, CharactorFaceNeutralPoseName, rootPath)
+    """
+    step2 在wrap中标记点并用生成FaceMarker文件并留意刚体对齐的点的索引使用matlab迁移
+    """
+    source_obj_path = os.path.join(rootPath, "Actor", "Tri", "Face", "NeutralPose", ActorFaceNeutralPoseName)
+    source_txt = os.path.join(rootPath, "Actor", "Tri", "Face", "NeutralPose", 'neural-source-points.txt')
+    target_obj_path = os.path.join(rootPath, "Charactor", "Tri", "Face", "NeutralPose", CharactorFaceNeutralPoseName)
+    target_txt = os.path.join(rootPath, "Charactor", "Tri", "Face", "NeutralPose", 'neural-target-points.txt')
+    file_mat_path = os.path.join(rootPath, "D:\\Origin_Deformaion_transfer", 'Face_Marker.mat')  # matlab facemarker 路径
+    # facemarker = make_Face_Marker(source_obj_path, source_txt, target_obj_path, target_txt, file_mat_path)
+    # rigid_index = [0, 1, 2]
+    # rigid_align_points_index = facemarker[np.array(rigid_index), 1] - 1
+    # print("刚体对齐使用的点序为{}".format(rigid_align_points_index))
+
+    """
+    step3 将迁移完成的面部数据安回到原有的头部上面 align and generate quads
+    """
+    Charactor_transfered_face_path = "D:\\Blendshape-Animation\\Transfered\\Small_sister1\\Charactor\\Tri\\Face\\Blendshapes"
+    aligned_face = os.path.join(rootPath, "Charactor", "Tri", "Face", "NeutralPose", "Xiaoyue.face.obj")
+    face_index_path = os.path.join(rootPath, "Charactor", "Tri", "Face", "NeutralPose", "face_index.pkl")
+    Charactor_head = os.path.join(rootPath, "Charactor", "Tri", "Head", "NeutralPose", CharactorHeadNeutralPoseName)
+    index_path = os.path.join(rootPath, "Charactor", "Tri", "Head", "NeutralPose", "index.pkl")
+    AlignPoints_index = [10406, 5064, 1386]
+    AlignedFaces_path_tosave = os.path.join(rootPath, "Charactor", "Tri", "Face", "AlignedBlendshapes")
+    Quad_Head_path_tosave = os.path.join(rootPath, "Charactor", "Quad")
+    Tri_Head_path_tosave = os.path.join(rootPath, "Charactor", "Tri", "Head", "Blendshapes")
+    BasicBlenshapeTransfer_step3(Charactor_transfered_face_path, aligned_face, face_index_path, Charactor_head,
+                                 index_path, AlignPoints_index, AlignedFaces_path_tosave,
+                                 Tri_Head_path_tosave, Quad_Head_path_tosave)
 
     """
     显示
