206f1e26ffef8b9b78d7690bf857d557e5e791f6
LiyouWang <108506687@qq.com>
2019-04-26 17:48:04 +0800

    blender下实现脚本加载obj序列，并对每个obj平滑同时保存选中的对象

---
+++ loadobjandsmooth.py
@@ -0,0 +1,15 @@
+import bpy
+
+root_path = "\\\\192.168.80.195\data3\LiyouWang"
+model_path = os.path.join(root_path, "AlignedHead")
+output_path = os.path.join(root_path, "smooth_head")
+
+objs = os.listdir(model_path)
+for obj in objs:
+    bpy.ops.import_scene.obj(filepath=os.path.join(model_path, obj))
+    bpy.context.scene.objects.active=bpy.data.objects[obj[:-4]]
+    bpy.ops.object.mode_set(mode='EDIT', toggle=False)
+    bpy.ops.mesh.vertices_smooth(repeat=2)
+    bpy.ops.object.mode_set(mode='OBJECT', toggle=False)
+    bpy.ops.export_scene.obj(filepath=os.path.join(output_path, obj), use_selection=True)
+    bpy.ops.object.delete()
