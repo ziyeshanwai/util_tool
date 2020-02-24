8f5aa24e734eca87decdff08e3a56f92f7108326
LiyouWang <1085066875@qq.com>
2020-02-24 11:53:10 +0800

    增加断言检测，确保排序后的索引长度与不排序的索引长度一致

--- blender_sort_selected_vertex.py
+++ blender_sort_selected_vertex.py
@@ -24,6 +24,7 @@ shape = (len(bpy.context.active_object.data.vertices) ,3)
 # method 1
 print("-------------method 1-------------------")
 inds = [i.index for i in bpy.context.active_object.data.vertices if i.select]  
+print("selected inds is {}".format(len(inds)))
 co = [i.co for i in bpy.context.active_object.data.vertices if i.select]
 bpy.context.active_object.data.vertices.foreach_get('co', verts)  # get
 verts.shape = shape
@@ -36,9 +37,12 @@ tmp_one = np.ones(len(contour))
 hmo_contour = np.c_[contour, tmp_one]
 contour_world_coor = np.matmul(obj.matrix_world, hmo_contour.T)
 contour_world_coor = contour_world_coor.T[:, 0:3]
-inds = np.array(inds, dtype=np.int32)[contour_world_coor[:,0].argsort()].tolist()
-print("inds is {}".format(inds))
+sorted_inds = np.array(inds, dtype=np.int32)[contour_world_coor[:,0].argsort()].tolist()
+assert len(sorted_inds) == len(inds), "len sorted inds is not equal inds"
+print("sorted_inds is {}".format(sorted_inds))
+print("method 1 len(sorted_inds) is {}".format(len(sorted_inds)))
 print("co is {}".format(contour_world_coor))
+save_pickle_file(r"D:\pycharm_project\snap_solver\blender_project\eye_right_up_contour.pkl", sorted_inds)
 
 
 
@@ -46,6 +50,5 @@ print("co is {}".format(contour_world_coor))
 print("------------------method2-----------------")
 inds = [i.index for i in bmesh.from_edit_mesh(bpy.context.active_object.data).verts if i.select]
 print("--------------------")
-print(len(inds))
+print("method2 len(inds) is {}".format(len(inds)))
 print(inds)
-save_pickle_file("./mouth_down_contour.pkl", inds)
