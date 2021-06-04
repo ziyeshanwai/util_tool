7c35413a3d71248d0baf6ee5c2ea84d6d8edfbe0
liyouwang <1085066875@qq.com>
2021-06-04 18:39:05 +0800

    导出shapekey value的另一种方式

--- blender2.92showshapkevalue.py
+++ blender2.92showshapkevalue.py
@@ -23,7 +23,31 @@ def export_metahuman_json(ob, outputpath):
                     
                     print("block : {}, tmp:{}".format(block, tmp))
     print("export over!")
+
+def export_metahuman_json2(ob, outputpath):
     
+    """
+    export metahuman curve to json
+    export the selected frame start frame end curve
+    """
+    dict_channel = {}
+    for i in range(bpy.context.scene.frame_start, bpy.context.scene.frame_end):
+        bpy.context.scene.frame_set(i)
+        for block in ob.data.shape_keys.key_blocks:
+            if block.name != "Basis":
+                tmp = {}
+                tmp["frameNum"] = i
+                tmp['value'] = block.value
+                if block.name in dict_channel.keys():
+                    dict_channel[block.name].append(tmp)
+                else:
+                    dict_channel[block.name] = []
+        print("export {}".format(i))
+#    print("export over!")
+    write_json(outputpath, dict_channel)
+
+
+
 if __name__ == "__main__":
    ob = bpy.data.objects['head_geo']
    outputpath = r""
