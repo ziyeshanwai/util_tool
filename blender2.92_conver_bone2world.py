559bdd31ae2038d47db2b33352e3b024a512938a
liyouwang <1085066875@qq.com>
2021-11-04 16:15:09 +0800

    已知骨骼名字，返回骨骼到世界坐标的变换矩阵，已知世界坐标的位置，pose模式下反算出指定骨骼位置

--- blender2.92_conver_bone2world.py
+++ blender2.92_conver_bone2world.py
@@ -17,20 +17,32 @@ def matrix_world(armature_ob, bone_name):
     basis = armature_ob.pose.bones[bone_name].matrix_basis
 
     parent = armature_ob.pose.bones[bone_name].parent
-    if parent is not None:
-        print("parent name is {}".format(parent.name))
     if parent == None:
         return  local @ basis
     else:
         parent_local = armature_ob.data.bones[parent.name].matrix_local
-        return matrix_world(armature_ob, parent.name) @ parent_local.inverted() @ local @ basis
+        return matrix_world(armature_ob, parent.name) @ (parent_local.inverted() @ local) @ basis
+
+def set_bone_basis(armature_ob, bone_name, world_basis):
+    """
+    set bone to world_basis
+    """
+    mat = matrix_world(armature_ob, bone_name)
+    basis = armature_ob.pose.bones[bone_name].matrix_basis
+    H = mat @ basis.inverted()
+    _basis = H.inverted() @ armature_ob.matrix_world.inverted() @ world_basis
+    armature_ob.pose.bones[bone_name].matrix_basis.translation = _basis.translation
+    return None
+
 
 empty_ob = bpy.data.objects["Cube"]
 armature_ob = bpy.data.objects["rest_pose"]
-
+world_basis = empty_ob.matrix_basis
 #bone_name = bpy.context.active_pose_bone.name
-bone_name = "head"
+bone_name = "neck_02"
+set_bone_basis(armature_ob, bone_name, world_basis)
+
 print(matrix_world(armature_ob, bone_name))
-print(worldMatrix2(armature_ob, bone_name))
-empty_ob.matrix_world = armature_ob.matrix_world @ matrix_world(armature_ob, bone_name)
+print(worldMatrix2(armature_ob,bone_name))
+#empty_ob.matrix_world = armature_ob.matrix_world @ matrix_world(armature_ob, bone_name)
 #empty_ob.matrix_world = worldMatrix(armature_ob, bone_name )
