fcc9bdf47b5b3aee79770b8614fc26b0e0890f50
Liyou <1085066875@qq.com>
2023-04-20 16:57:37 +0800

    读取json创建顶点组并给顶点组赋值

--- blender3.4_create_and_modify_weights.py
+++ blender3.4_create_and_modify_weights.py
@@ -13,3 +13,31 @@ def weight_changer(scene, depsgraph):
     new_vertex_group.add( vertex_indices, np.random.rand(1)[0], 'REPLACE' )
 
 bpy.app.handlers.frame_change_post.append(weight_changer)
+
+
+import bpy
+import json
+import os
+
+
+def _load_json(json_file):
+    """
+    load json file
+    :param json_file: json 文件路径
+    :return:
+    """
+    with open(json_file, 'r') as f:
+        data = json.load(f)
+    return data
+
+
+if __name__ == "__main__":
+    json_path = os.path.join(r"D:\pycharm_projects\BSGECMD\CODE\MASK", "CTRL_expressions_eyeBlinkL.json")
+    data = _load_json(json_path)
+    ob = bpy.context.active_object
+    new_vertex_group = ob.vertex_groups.new(name="CTRL_expressions_eyeBlinkL")
+    vertex_group_data = [i for i in range(0, len(ob.data.vertices))]
+    new_vertex_group.add(vertex_group_data, 1.0, 'ADD')
+#    vertex_indices = np.random.randint(0, 200, 200).tolist()
+    for i, v in enumerate(data['data']):
+        new_vertex_group.add( [i], v, 'REPLACE' )
